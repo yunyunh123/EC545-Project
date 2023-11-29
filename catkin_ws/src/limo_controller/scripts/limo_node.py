db88d6df7360f9dfbe99f41a3a93a8c0834b7af6
Julia Hua <jhua2@bu.edu>
2023-11-29 13:39:01 -0500

    fixed limo node

--- catkin_ws/src/limo_controller/scripts/limo_node.py
+++ catkin_ws/src/limo_controller/scripts/limo_node.py
@@ -3,37 +3,28 @@ import rospy
 from std_msgs.msg import String
 from pylimo import limo
 
-NODE = "limo_node"
 LIMO_ID = 1
 
-TOPIC_FOL = "/limo/follower_state"
-TOPIC_LDR = "/limo/leader_state"
+NODE = "limo_node"
+TOPIC = "/limo/state"
 QUEUE_SZ = 10
 RATE_HZ = 2
     
+def state_callback(msg: String):
+    print("Received: ", msg)
 
-def leader_state_callback(msg: String):
-    print(msg)
-    #leader_state = str(msg).split(";")
-    #ldr_id = leader_state[0].replace("data: ", "")#int(leader_state[0])
-    #lin_vel = float(leader_state[1])
-    #steer_angle = float(leader_state[2])
-    #print(leader_state) #ldr_id, lin_vel, steer_angle)
-    
 def getCurrentState(mylimo, id):
     lin_vel = mylimo.GetLinearVelocity()
-    ang_vel = mylimo.GetAngularVelocity()
     steer_angle = mylimo.GetSteeringAngle()
     return str(id) + ";" + str(lin_vel) + ";" + str(steer_angle)
-
 
 if __name__ == '__main__':
 
     # Set up ros
     rospy.init_node(NODE)
     rospy.loginfo("Limo node has been started.")
-    pub = rospy.Publisher(TOPIC_FOL, String, queue_size=QUEUE_SZ)
-    sub = rospy.Subscriber(TOPIC_LDR, String, callback=leader_state_callback)
+    pub = rospy.Publisher(TOPIC, String, queue_size=QUEUE_SZ)
+    sub = rospy.Subscriber(TOPIC, String, callback=state_callback)
     rate = rospy.Rate(RATE_HZ)
 
     # Set up limo
