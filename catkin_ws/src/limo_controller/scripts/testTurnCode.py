bb819b3caa0e6e5707b8552c83ddb9078e9865c3
Cole Wentzel <85313355+colewentzel@users.noreply.github.com>
2023-12-06 12:55:34 -0500

    steering angle code

--- catkin_ws/src/limo_controller/scripts/testTurnCode.py
+++ catkin_ws/src/limo_controller/scripts/testTurnCode.py
@@ -3,7 +3,7 @@ LEFT_SENSOR_VAL = .85956 # all positive values are left
 RIGHT_SENSOR_VAL = -0.5576 # all negative values are right
 
 TURN_ANGLE_RANGE = 35 # degrees
-TURN_CLOSEST_PERCENT = 10 # percent
+TURN_CLOSEST_PERCENT = 15 # percent
 
 '''
 Check if scan is done clockwise or counterclockwise.
@@ -15,22 +15,25 @@ def main(turnDistances):
 # ----- Turning implementation 
     # Declare variables for the implementation
     steeringMatrix = [0] * len(turnDistances) # array that holds steering angle with same indexes as the turn distances
-
-    # Calculate the degree each datapoint is relative
-    totalDegreesCovered = TURN_ANGLE_RANGE * 2
-    distBetweenMeasurements = totalDegreesCovered / (len(turnDistances) - 1)
+    numHalfTurnAngle = int(int(len(turnDistances))/2)
+    #rightSideSteeringAngles = [0] * (numHalfTurnAngle) # holds steering angles for turning right (0 to RIGHT_SENSOR_VAL)
+    #leftSideSteeringAngles = [0] * (numHalfTurnAngle) # holds steering angles for turning left (LEFT_SENSOR_VAL to 0)
+    
+    # Calculate the distances for the right and left sides of the data set
+    distBetweenMeasurementsLeft = LEFT_SENSOR_VAL / numHalfTurnAngle
+    distBetweenMeasurementsRight = RIGHT_SENSOR_VAL / numHalfTurnAngle
     
     # Calculate the steering angle towards each datapoint and insert into an array
-    turnAngle = TURN_ANGLE_RANGE * -1
+    turnAngle = LEFT_SENSOR_VAL
     for index, dataPoint in enumerate(turnDistances):
-        #MIGHT NEED TO REVERSE THESE IF LIDAR GETS DATA COUNTERCLOCKWISE
-        halfTurnAngle = int(int(len(turnDistances))/2)
-        rightSideSteeringAngles = [0] * (halfTurnAngle) # holds steering angles for turning right
-        leftSideSteeringAngles = [0] * (halfTurnAngle) # holds steering angles for turning left
+        steeringMatrix[index] = turnAngle
 
-        # REPLACE WITH CALCULATED EQUATION RESULT - Currently saves degrees of each value
-        steeringMatrix[index] = turnAngle 
-        turnAngle = turnAngle + distBetweenMeasurements
+        if index < numHalfTurnAngle: # handle the left side
+            turnAngle = turnAngle - distBetweenMeasurementsLeft
+        elif index > numHalfTurnAngle: # handle the right side
+            turnAngle = turnAngle + distBetweenMeasurementsRight
+        elif index == numHalfTurnAngle: # handle the center
+            turnAngle = 0
 
     # Calculate the what datapoints are the closest (ex. 90% closest datapoints - FINE TUNE PERCENTAGE)
     numCloseValues = int((TURN_CLOSEST_PERCENT/100) * len(turnDistances)) # number of values in the top * percent
@@ -49,7 +52,7 @@ def main(turnDistances):
     return averageSteeringAngle # Return the steering angle based on LiDAR data
 
 if __name__ == '__main__':
-    turnDistances = [10.1, 10.2, 12.5, 20.3, 20.0, 21.1, 21.1, 10.0, 10.0, 10.3, 11.2, 13.1, 16.4, 20.1, 21.2, 6.3, 3.1, 4.1, 5.0, 4.1, 6.2, 7.3]
+    turnDistances = [10.1, 10.2, 12.5, 20.3, 20.0, 21.1, 21.1, 10.0, 10.0, 10.1, 10.2, 10.3, 11.2, 13.1, 16.4, 20.1, 21.2, 6.3, 3.1, 4.1, 5.0, 4.1, 6.2, 7.3]
     
     result = main(turnDistances)
     print(result)
